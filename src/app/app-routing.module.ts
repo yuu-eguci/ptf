b52faf05538dea0d2d8d1e80fb2365fd1b6149e1
yuu-eguci <yuu.eguci@gmail.com>
2020-02-01 19:35:02 +0900

    Add artworks-page base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EngineeringPageComponent } from './engineering-page/engineering-page.component';
+import { ArtworksPageComponent } from './artworks-page/artworks-page.component';
 
 
 const routes: Routes = [
   { path: '', component: EngineeringPageComponent },
+  { path: 'artworks', component: ArtworksPageComponent },
 ];
 
 @NgModule({
