99ff957a89b8b97d053fed126383aaa82380e4ad
yuu-eguci <yuu.eguci@gmail.com>
2020-02-02 22:22:50 +0900

    #8 Use ProgressComponent from OldPtfPageComponent

--- src/app/progress/progress.component.ts
+++ src/app/progress/progress.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-progress',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./progress.component.sass']
 })
 export class ProgressComponent implements OnInit {
+
+  @Input() bgType: string = '';
+  @Input() value: string = '';
+  @Input() name: string = '';
 
   constructor() { }
 
