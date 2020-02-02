f1e847d153481b07e59182bec5dd6ea5507b1006
yuu-eguci <yuu.eguci@gmail.com>
2020-02-02 21:23:17 +0900

    #8 Use HeroAreaComponent from OldPtfPageComponent

--- src/app/hero-area/hero-area.component.ts
+++ src/app/hero-area/hero-area.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-hero-area',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-area.component.sass']
 })
 export class HeroAreaComponent implements OnInit {
+
+  // FIXME: Although I want to specify image path from parent component, 'sanitizing unsafe style value' error occurs.
+  @Input() backgroundImagePath: string = '';
 
   constructor() { }
 
