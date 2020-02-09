a9a71c7a551034edd32f0fd6bdb91165ec45bd37
yuu-eguci <yuu.eguci@gmail.com>
2020-02-09 09:13:36 +0900

    #15 Use [ngStyle] in breadcrumb.component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { OldPtfPageComponent } from './old-ptf-page/old-ptf-page.component';
 import { EngineeringPageComponent } from './engineering-page/engineering-page.component';
 import { ArtworksPageComponent } from './artworks-page/artworks-page.component';
 import { IndexPageComponent } from './index-page/index-page.component';
 
 
 const routes: Routes = [
-  { path: '', component: OldPtfPageComponent },
-  { path: 'index', component: IndexPageComponent },
+  { path: '', component: IndexPageComponent },
   { path: 'engineering', component: EngineeringPageComponent },
   { path: 'artworks', component: ArtworksPageComponent },
 ];
