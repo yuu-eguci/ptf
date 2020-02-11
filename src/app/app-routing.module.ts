970b50ddc1d1b4018218f690e901023ebe1ad22d
yuu-eguci <yuu.eguci@gmail.com>
2020-02-11 15:15:47 +0900

    #22 Implement journey page roughly

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { EngineeringPageComponent } from './engineering-page/engineering-page.component';
 import { ArtworksPageComponent } from './artworks-page/artworks-page.component';
 import { IndexPageComponent } from './index-page/index-page.component';
+import { JourneyPageComponent } from './journey-page/journey-page.component';
 
 
 const routes: Routes = [
   { path: '', component: IndexPageComponent },
   { path: 'engineering', component: EngineeringPageComponent },
   { path: 'artworks', component: ArtworksPageComponent },
+  { path: 'journey', component: JourneyPageComponent },
 ];
 
 @NgModule({
