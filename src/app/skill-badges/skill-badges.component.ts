da57f5627e022ad8f9de47045922a557059ef489
yuu-eguci <yuu.eguci@gmail.com>
2020-02-16 11:28:54 +0900

    #36 Update skill-badges.component

--- src/app/skill-badges/skill-badges.component.ts
+++ src/app/skill-badges/skill-badges.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-skill-badges',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skill-badges.component.sass']
 })
 export class SkillBadgesComponent implements OnInit {
+
+  @Input() lis: {}[] = [];
 
   constructor() { }
 
