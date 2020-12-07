b93692204cf4d5274d4e4fbd405108e14971c936
yuu-eguci <yuu.eguci@gmail.com>
2020-12-07 15:57:13 +0900

    ✨ Activate .portfolio-img also in the engineering page

--- src/app/engineering-page/engineering-page.component.ts
+++ src/app/engineering-page/engineering-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+import 'magnific-popup';
 
 @Component({
   selector: 'app-engineering-page',
@@ -151,6 +153,20 @@ export class EngineeringPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+
+    $('.portfolio-img').magnificPopup({
+      type: 'image',
+      gallery: {
+        enabled: true,
+        preload: [0, 2],
+        navigateByImgClick: true,
+        tPrev: 'Previous',
+        tNext: 'Next'
+      }
+    });
   }
 
 }
